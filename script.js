3c0ef2c9113c6341139cf998c6320bb09b3241af
[derealOG] <[derealOG001@gmail.com]>
2022-07-24 01:40:31 -1000

    commit

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const dropDownContent = document.getElementsByClassName("profileDropdown")[0];
-const dropDownBtn = document.getElementsByClassName("down")[0];
 
-dropDownBtn.onclick = function () {
-  dropDownContent.classList.toggle(".show");
-};
